519e4ec9ed2b5a051eddcd95a864f418e449ec1e
Andy Howell <andyhowell@MacBook-Pro-2.local>
2024-09-20 12:12:00 +0100

    adding base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig, squooshImageService } from 'astro/config';
 
 export default defineConfig({
   site: 'https://spacebeers.github.io',
+  base: '/atomic-horse-astro',
   image: {
     service: squooshImageService()
   },
