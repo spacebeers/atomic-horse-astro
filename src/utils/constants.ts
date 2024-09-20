a1e5f9e52c4c9ca1ddf4b6020cb3ee7dbf17ca4e
Andy Howell <andyhowell@MacBook-Pro-2.local>
2024-09-20 10:33:27 +0100

    more skeleton work

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const EMAIL = "hello@atomichorse.agency"
+export const CALENDLY = "https://calendly.com/hello-atomichorse"
 export const PHONE = "07917 414088"
 export const BASE = "/atomic-horse-astro"
