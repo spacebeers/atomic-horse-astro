4c70cc1df28151eaf26c4c03facdecb670b066fa
Andy Howell <andyhowell@MacBook-Pro-2.local>
2024-09-23 13:44:28 +0100

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig, squooshImageService } from 'astro/config';
 
 export default defineConfig({
   site: 'https://spacebeers.github.io',
-  // base: '/atomic-horse-astro',
+  base: '/atomic-horse-astro',
   image: {
     service: squooshImageService()
   },
