32e3e68cb6b05c400d15c4ad59325144d85c6dfd
Andy Howell <andyhowell@MacBook-Pro-2.local>
2024-09-24 22:23:55 +0100

    Adding posts and site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { loadEnv } from "vite";
 import { defineConfig, squooshImageService } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://spacebeers.github.io',
   base: '/atomic-horse-astro',
+  integrations: [sitemap()],
   image: {
     service: squooshImageService()
-  },
+  }
 });
