1809457f822ca2fa2fa9e19d9487f65b5eccd3a4
Andy Howell <andyhowell@MacBook-Pro-2.local>
2024-09-26 22:20:31 +0100

    adding services section content collection

--- src/content/content.ts
+++ src/content/content.ts
@@ -29,7 +29,21 @@ const blogCollection = defineCollection({
   }),
 });
 
+const serviceCollection = defineCollection({
+  type: 'content', // v2.5.0 and later
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
+    imageAlt: z.string(),
+    description: z.string(),
+  }),
+});
+
 export const collections = {
   'work': workCollection,
   'blog': blogCollection,
+  'service': serviceCollection,
 };
