d633c3a9e934095c7e5acebe09f96e8f4903b9a0
Andy Howell <andyhowell@MacBook-Pro-2.local>
2024-09-28 10:35:22 +0100

    fix(): Build issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://spacebeers.github.io',
-  base: '/atomic-horse-astro',
-  integrations: [sitemap()],
-  image: {
-    service: squooshImageService()
-  }
+  integrations: [sitemap()]
 });
