bee1a7570f3b4a7bbc519c03dd281a8f84d6dd6d
Andy Howell <andyhowell@MacBook-Pro-2.local>
2024-09-28 11:28:31 +0100

    adding site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: "https://atomic-horse-astro-lchgumj2n-helloatomichorses-projects.vercel.app/",
   output: 'hybrid',
   adapter: vercel(),
   integrations: [sitemap()]
