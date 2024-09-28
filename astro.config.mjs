dbda21a6787a3a2578344509af4b8753496aa51b
Andy Howell <andyhowell@MacBook-Pro-2.local>
2024-09-28 08:16:16 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig, squooshImageService } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import formDebug from "@astro-utils/forms/dist/integration.js";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://spacebeers.github.io',
   base: '/atomic-horse-astro',
   integrations: [sitemap()],
   image: {
     service: squooshImageService()
-  },
-  output: 'server',
-  integrations: [formDebug]
+  }
 });
