e9b1fd4403427735fb040d62932df4ac58b87702
Andy Howell <andyhowell@MacBook-Pro-2.local>
2024-09-29 17:53:06 +0100

    more image conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
+import { ImageService } from 'astro';
 
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  adapter: netlify({
+    ImageService: true
+  }),
   site: "https://atomichorsedigital.netlify.app",
   image: {
     domains: ['netlify.app']
