4aa494bfa12713fb23d626d5a93018959e2d5594
Andy Howell <andyhowell@MacBook-Pro-2.local>
2024-09-29 07:40:16 +0100

    fix(): Trying netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: "https://atomic-horse-astro-lchgumj2n-helloatomichorses-projects.vercel.app/",
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
   integrations: [sitemap()]
 });
