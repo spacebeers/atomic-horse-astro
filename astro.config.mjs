8d881e566d85774037b2aca55fe072431ebe4be3
Andy Howell <andyhowell@MacBook-Pro-2.local>
2024-09-29 07:49:43 +0100

    updating url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: "https://marvelous-bubblegum-768f00.netlify.app/",
+  site: "https://atomichorsedigital.netlify.app/",
   output: 'server',
   adapter: netlify(),
   integrations: [sitemap()]
