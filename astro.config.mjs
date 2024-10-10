6a6e3ab28db4abe179fe8c0da464490a23a0769f
Andy Howell <andyhowell@MacBook-Pro-2.local>
2024-10-10 07:36:44 +0100

    fix(): More image crap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,14 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  output: 'hybrid',
-  adapter: netlify({
-    assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg,', '**/*.jpeg'],
-  }),
-  site: "https://atomichorsedigital.netlify.app",
-  image: {
-    domains: ['netlify.app']
-  },
-  integrations: [sitemap(), mdx()]
+    output: 'hybrid',
+    adapter: netlify({
+        imageCDN: false,
+        assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg,', '**/*.jpeg'],
+    }),
+    site: "https://atomichorsedigital.netlify.app",
+    image: {
+        domains: ['netlify.app']
+    },
+    integrations: [sitemap(), mdx()]
 });
