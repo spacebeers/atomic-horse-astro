cb58527732383ff1ae4521ce3017c491f6650419
Andy Howell <andyhowell@MacBook-Pro-2.local>
2024-11-22 08:02:13 +0000

    fix(): theme scrolls

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,9 +1,10 @@
-export const setTheme = () => {
+export const setTheme = (value: string) => {
     const themeMetaTag = document.querySelector('meta[name="theme-color"]');
     
     if (themeMetaTag) {
       const rootStyles = getComputedStyle(document.documentElement);
-      const highlightColor = rootStyles.getPropertyValue('--palette-highlight').trim();
+      const c = value === 'highlight' ? '--palette-highlight' : '--palette-dark';
+      const highlightColor = rootStyles.getPropertyValue(c).trim();
 
       if (highlightColor) {
         themeMetaTag.setAttribute('content', highlightColor);
